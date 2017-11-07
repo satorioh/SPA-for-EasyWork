9f2e096662ec49fd2c5e3a9f37e65f6bc5be1cbd
satorioh <wangbinxp@gmail.com>
2017-11-07 17:53:53 +0800

    预览部分图片设置完成

---
+++ js/my.js
@@ -0,0 +1,34 @@
+// Gallery slider
+$('.carousel-slider-content').each(function() {
+  $(this).slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    draggable: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          draggable: true
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          arrows: true,
+          dots: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
